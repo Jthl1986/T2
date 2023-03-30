5887c5c51e6c4093d391dc95a7db9f6ca13b81ed
Santacecilia <jorgesantacecilia@gmail.com>
2023-03-30 14:42:56 -0300

    9

--- app3.py
+++ app3.py
@@ -370,7 +370,7 @@ def app5():
     if dfa is not None:
         right.subheader("🐮 Existencias de hacienda")
         right.table(dfa.style.format({"Cantidad":"{:.0f}", "Peso":"{:.0f}", "Valuación":"${:,}"}))
-    
+    #topLeftMargin * 20 es donde manejas el ancho
     if st.button(BUTTON_TEXT):
         components.html(
                     f"""
@@ -389,7 +389,7 @@ def app5():
         const docWidth = stApp.scrollWidth;
         
         let topLeftMargin = 30;
-        let pdfWidth = docHeight + (topLeftMargin * 30);
+        let pdfWidth = docHeight + (topLeftMargin * 20);
         let pdfHeight = (pdfWidth * 1.5) + (topLeftMargin * 2);
         let canvasImageWidth = docWidth;
         let canvasImageHeight = docHeight;
