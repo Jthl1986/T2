616bceb70638e88c26de7fd04d2d6384302d629e
Santacecilia <jorgesantacecilia@gmail.com>
2023-03-30 14:37:39 -0300

    9

--- app3.py
+++ app3.py
@@ -390,7 +390,7 @@ def app5():
         
         let topLeftMargin = 30;
         let pdfWidth = docHeight + (topLeftMargin * 2);
-        let pdfHeight = (pdfWidth * 2) + (topLeftMargin * 3);
+        let pdfHeight = (pdfWidth * 1.5) + (topLeftMargin * 3);
         let canvasImageWidth = docWidth;
         let canvasImageHeight = docHeight;
         
